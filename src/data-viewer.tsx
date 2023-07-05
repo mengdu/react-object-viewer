4ec01a98b69d247f314a0abb3bc301ef43c72072
jane <lanyueos@qq.com>
2023-07-05 18:46:16 +0800

    init code

---
+++ src/data-viewer.tsx
@@ -0,0 +1,8 @@
+export function DataViewer ({ data }: { data: any }) {
+  console.log(data)
+  return (
+    <div className="react-data-viewer">
+      data
+    </div>
+  )
+}
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export * from './data-viewer'
+import './style.less'
